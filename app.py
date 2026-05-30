1822af5e769a343e48419f561745bd0421186bd3
Gamze MADEN <81433698+gamzemdn@users.noreply.github.com>
2026-05-30 03:09:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3760,5 +3760,5 @@ register_example_workflows(
     }
 )
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=8040)
+    app.run(host="0.0.0.0", debug=False, port=8040)
     #app.run_server(debug=True, port=8040)
